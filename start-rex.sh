dd95a1a46268434f229d189214cdc2bcdda017be
Fahim Shariar <fahim.shoumik@gmail.com>
2017-10-25 14:05:29 +0600

    minio config updated

--- start-rex.sh
+++ start-rex.sh
@@ -4,7 +4,6 @@ cd /etc/rexray/
 sed -i "s|S3FS_ENTRY|$S3FS_ENTRY|g" /etc/rexray/config.yml
 sed -i "s/S3FS_ACCESSKEY/$S3FS_ACCESSKEY/g" /etc/rexray/config.yml
 sed -i "s/S3FS_SECRETKEY/$S3FS_SECRETKEY/g" /etc/rexray/config.yml
-sed -i "s/S3FS_OPTIONS/$S3FS_OPTIONS/g" /etc/rexray/config.yml
 
 /usr/bin/rexray version
 /usr/bin/rexray start --logLevel=info --nopid
